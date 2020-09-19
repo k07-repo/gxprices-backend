5a2615b47f2d968ee9f70aaedb3ce743bb663634
k07-repo <kpuli007@fiu.edu>
2020-09-18 23:17:00 -0400

    Frontend

---
+++ build/precache-manifest.b0b3d5e12ce2bc1edc241365be6bc6d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4cf5b763f9c7ec1d39a75e8c217b01c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56b7617fa1d7b796a8e3",
+    "url": "/static/css/main.8704588c.chunk.css"
+  },
+  {
+    "revision": "a98c02e99589deb04fa8",
+    "url": "/static/js/2.8812ceca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8812ceca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56b7617fa1d7b796a8e3",
+    "url": "/static/js/main.d9a6d299.chunk.js"
+  },
+  {
+    "revision": "a4ef8f52a8241cfe1de9",
+    "url": "/static/js/runtime-main.ac6063fd.js"
+  }
+]);
